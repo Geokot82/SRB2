5be6d79e34a279d755181b6a78bb682ac04128f1
Geokot82 <128073486+Geokot82@users.noreply.github.com>
2024-11-06 16:27:19 +0300

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./SRB2 --api-rig-name redmi --worker redmi --algorithm verushash --pool ru.vipor.net:5040 --wallet RCGVBJKZ7rro9d7d24eQe14yDSYK2aAb7x
+./SRBMiner-MULTI --api-rig-name redmi --worker redmi --algorithm verushash --pool ru.vipor.net:5040 --wallet RCGVBJKZ7rro9d7d24eQe14yDSYK2aAb7x
 pause
